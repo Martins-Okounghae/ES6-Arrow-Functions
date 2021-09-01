298b26680a445f7c33b28dba9fb5556dc40896f2
Martins-Okounghae <martins.okuonghae@outlook.com>
2021-09-01 23:46:23 +0100

    Implemented fat arrow in App.jsx

--- src/index.js
+++ src/index.js
@@ -12,15 +12,13 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 ////Filter - Create a new array by keeping the items that return true.
-const newNumbers = numbers.filter((num) => num < 10);
+//const newNumbers = numbers.filter((num) => num < 10);
 
 ////Reduce - Accumulate a value by doing something to each item in an array.
-var newNumber = numbers.reduce(
-  (accumulator, currentNumber) => accumulator + currentNumber
-);
+// var newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
 
-////Find - find the first item that matches from an array.
-const newNumber2 = numbers.find((num) => num > 10);
+// ////Find - find the first item that matches from an array.
+// const newNumber2 = numbers.find((num) => num > 10);
 
-//FindIndex - find the index of the first item that matches.
-const newNumber3 = numbers.findIndex((num) => num > 10);
+// //FindIndex - find the index of the first item that matches.
+// const newNumber3 = numbers.findIndex((num) => num > 10);
